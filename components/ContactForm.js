e214238c0105ff8e32215cfa2bda95f2d0ddffd3
Todd Buchanan <todd@tbuchanan.com>
2023-01-23 20:50:27 -0500

    Contact form changes

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import { useRouter } from 'next/router'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 export default function ContactForm() {
+  const [state, setState] = useState({})
+
+  const router = useRouter()
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => router.push(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
   return (
     <div className="flex flex-col md:flex-row text-left">
       <div className="bg-amber-400 flex flex-1 flex-col justify-center px-4 py-8 sm:py-4">
@@ -6,21 +38,26 @@ export default function ContactForm() {
         <p>Have a project or just want to dicuss an idea? Fill out the form to get started.</p>
       </div>
       <div className="flex-grow">
-      <form name="contact" action="/success" method="POST" netlify>
+      <form name="contact" action="/success" method="POST" netlify data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
         <div className="p-6">
           <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+          </p>
           <div className="">
             <label htmlFor="full-name" className="block text-sm font-medium text-gray-700">Full Name</label>
-            <input type="text" name="full-name" id="full-name" autocomplete="given-name" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" />
+            <input type="text" name="full-name" id="full-name" autocomplete="given-name" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" onChange={handleChange}/>
           </div>
           <div className="mt-6">
             <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">Email</label>
-            <input type="text" name="email-address" id="email-address" autocomplete="email" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" />
+            <input type="text" name="email-address" id="email-address" autocomplete="email" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" onChange={handleChange}/>
           </div>
           <div className=" mt-6">
             <label htmlFor="form-message" className="block text-sm font-medium text-gray-700">Message</label>
             <div className="mt-1">
-              <textarea id="form-message" name="form-message" rows="5" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" placeholder="What would you like to discuss?" />
+              <textarea id="form-message" name="form-message" rows="5" className="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" placeholder="What would you like to discuss?" onChange={handleChange}/>
             </div>
           </div>
         </div>
